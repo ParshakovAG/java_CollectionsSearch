439f2c09cc0ddd9249ccde31faec2ec598c45d0f
Sasha <sash29@yandex.ru>
2022-07-18 22:02:23 +0500

    generateCoolNumbers done

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,5 +1,9 @@
 package practice;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Main {
     /*
     TODO:
@@ -7,8 +11,26 @@ public class Main {
      - посчитать время поиска введимого номера в консоль в каждой из структуры данных
      - проанализоровать полученные данные
      */
-
     public static void main(String[] args) {
+        List<String> testList = CoolNumbers.generateCoolNumbers();
+
+        System.out.println("Сгенерирован список автомобильных номеров, в размере "
+                + testList.size() + " единиц");
+
+
+        System.out.println();
+        CoolNumbers.bruteForceSearchInList(testList, "А001АА199");
+        System.out.println();
+        CoolNumbers.binarySearchInList(testList, "А001АА199");
+        System.out.println();
+
+
+        HashSet<String> testHash = new HashSet<>(testList);
+        CoolNumbers.searchInHashSet(testHash, "А001АА199");
+        System.out.println();
+        TreeSet<String> testTreeSet = new TreeSet<>(testList);
+        CoolNumbers.searchInTreeSet(testTreeSet, "А001АА199");
+        System.out.println();
 
     }
 }
